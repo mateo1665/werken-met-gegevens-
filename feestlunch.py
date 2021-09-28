738e62c19cb452724a9c816d54348ee128076758
mateo1665 <89844584+mateo1665@users.noreply.github.com>
2021-09-28 09:42:57 +0200

    Add files via upload

---
+++ feestlunch.py
@@ -0,0 +1,16 @@
+croisantjes = 17
+eencroisantprijs = 0.39 
+stokbrooden = 2
+eenstokbroodprijs = 2.78
+kortingsbonnen = 3
+eenkortingsbon = 0.50
+totalprijscroisanjes = 6.63
+totalprijsstokbrooden = 5.56
+totalprijskortingsbonnen = 1.50
+
+print ( croisantjes * eencroisantprijs) 
+print (stokbrooden * eenstokbroodprijs)
+print (kortingsbonnen * eenkortingsbon)
+print (totalprijsstokbrooden + totalprijscroisanjes - totalprijskortingsbonnen)
+
+total = 10.69
---
+++ speelhaldag.py
@@ -0,0 +1,14 @@
+personen = 3
+prijsperpersoon = 7.45
+VRminuten = 5
+prijsVRminuten = 0.37
+totalVRminuten = 45
+totalprijsperpersoon = 22.35
+totalprijsVRminuten = 3.33
+
+print (personen * prijsperpersoon)
+print (VRminuten * 9)
+print (prijsVRminuten * 9)
+print (totalprijsperpersoon + totalprijsVRminuten)
+
+total = 25.68
